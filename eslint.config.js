7154b261a02246e3c80528e61adfb9feacb2d935
gidolee <39383541+gidolee@users.noreply.github.com>
2026-03-15 08:11:24 +0000

    updated eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,7 @@
 import tsParser from '@typescript-eslint/parser';
 
-export default [
-    {
-        rules: {
-            semi: 'error',
-            'prefer-const': 'error',
-        },
-    },
-];
+export default {
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+};
