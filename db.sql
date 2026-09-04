c7e7ac6eb87c5470049102f55e6e6dca16c02b70
courage173 <courageosemwengie@gmail.com>
2026-09-04 12:18:53 +0200

    update db.sql and migrations/1775457821969_init-users-table.js

--- db.sql
+++ db.sql
@@ -3,11 +3,11 @@ CREATE TABLE users (
   first_name VARCHAR(50) NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   email VARCHAR(100) UNIQUE NOT NULL,
-  password VARCHAR(25) NOT NULL,
+  password VARCHAR(255) NOT NULL,
   gender VARCHAR(20),
   job_role VARCHAR(100),
-  department VARCHAR(100),
-  address VARCHAR(255),
+  department VARCHAR(255),
+  address TEXT,
   is_admin BOOLEAN DEFAULT FALSE,
   created_on TIMESTAMP DEFAULT NOW()
 );
--- migrations/1775457821969_init-users-table.js
+++ migrations/1775457821969_init-users-table.js
@@ -5,10 +5,10 @@ exports.up = (pgm) => {
       first_name VARCHAR(50) NOT NULL,
       last_name VARCHAR(50) NOT NULL,
       email VARCHAR(100) UNIQUE NOT NULL,
-      password VARCHAR(50) NOT NULL,
+      password VARCHAR(255) NOT NULL,
       gender VARCHAR(20),
       job_role VARCHAR(100),
-      department VARCHAR(100),
+      department VARCHAR(255),
       address TEXT,
       is_admin BOOLEAN DEFAULT FALSE,
       created_on TIMESTAMP DEFAULT NOW()
